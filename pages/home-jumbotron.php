158070df96ab2d9ddf7f0c801ab59db1d042aaa1
wi20b063 <84073745+wi20b063@users.noreply.github.com>
2022-10-21 12:36:06 +0200

    basic structure 1

---
+++ pages/home-jumbotron.php
@@ -0,0 +1,13 @@
+
+
+<!--basic concept of a jubotron to integrate the big photo at the homepage-->
+<div class="jumbotron" id="jumbotron-home">
+  <h1 class="display-4">Willkomen im Distant Hotel!</h1>
+  <h2 class="lead slogan"></h2>
+  <h2 class="lead slogan">Your HOME Away From HOME</p>
+  <hr class="my-4">
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium sit repellat excepturi, beatae delectus soluta et modi incidunt aliquam.</p>
+  <p class="lead">
+    <a class="btn btn-primary btn-lg" href="#" role="button">Mehr erfahren</a>
+  </p>
+</div>
