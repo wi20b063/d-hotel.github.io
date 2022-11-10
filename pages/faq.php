6bc30dbbb73ab4737f50a6215a2466ac7feb8e82
distant-hotel <91386847+distant-hotel@users.noreply.github.com>
2022-11-10 09:58:49 +0100

    Faq & css update

---
+++ pages/faq.php
@@ -0,0 +1,102 @@
+<link rel="stylesheet" type="text/css" href="res/css/mystyle.css">
+
+
+<section>
+    <div class="container">
+        <div class="accordion">
+            <div class="accordion-item" id="question1">
+                <a class="accordion-link" href="#question1">
+                    Wie können Reservierungen vorgenommen werden?
+                    <i class="icon ion-md-add"></i>
+                    <i class="icon ion-md-remove"></i>
+                </a>
+                <div class="answer">
+                    <p>
+                        Nachdem die Verfügbarkeit des Zimmers geprüft wurde, werden Sie gebeten, Ihre Daten in unser
+                        Online-Reservierungsformular einzugeben. Sobald das System Ihre Reservierung bestätigt hat,
+                        erhalten
+                        Sie
+                        eine E-Mail-Nachricht mit der eigentlichen Bestätigung einschließlich der Reservierungsnummer.
+                        Sie sind jedoch auch dabei, ein Zimmer in unserem Hotel zu buchen, indem Sie sich an unseren
+                        Kollegen im
+                        Distant Hotel wenden. Sie können die Bestätigung Ihrer Buchung per Fax oder E-Mail
+                        anfordern.
+                    </p>
+                </div>
+            </div>
+
+            <div class="accordion-item" id="question2">
+                <a class="accordion-link" href="#question2">
+                    Wozu werden meine Kreditkartendetails benötigt?
+                    <i class="icon ion-md-add"></i>
+                    <i class="icon ion-md-remove"></i>
+                </a>
+                <div class="answer">
+                    <p>
+                        Gemäß unserer Hotelrichtlinie werden die Kreditkartendaten als Garantie gegen Nichterscheinen
+                        durch
+                        den
+                        Kunden verwendet, oder das Hotel garantiert, dass die Buchung unseres Kunden bis zu Ihrer
+                        Ankunft
+                        gehalten wird. Beim Check-in hat man die Möglichkeit, die Buchung in bar oder mit einer anderen
+                        Kreditkarte zu bezahlen.
+                    </p>
+                </div>
+            </div>
+
+            <div class="accordion-item" id="question3">
+                <a class="accordion-link" href="#question3">
+                    Wie sicher ist die Eingabe meiner persönlichen Daten auf Ihrer Website?
+                    <i class="icon ion-md-add"></i>
+                    <i class="icon ion-md-remove"></i>
+                </a>
+                <div class="answer">
+                    <p>
+                        Der Schutz der Daten unserer Kunden ist uns sehr wichtig. Ihre persönlichen Daten werden durch
+                        einen
+                        sicheren Server geschützt. Wir arbeiten mit der sogenannten SSL-Technologie (Secure Sockets
+                        Layer),
+                        die
+                        alle Daten zunächst verschlüsselt, bevor sie an unseren Server gesendet werden.
+                    </p>
+                </div>
+            </div>
+
+            <div class="accordion-item" id="question4">
+                <a class="accordion-link" href="#question4">
+                    Stornierung einer Reservierung
+                    <i class="icon ion-md-add"></i>
+                    <i class="icon ion-md-remove"></i>
+                </a>
+                <div class="answer">
+                    <p>
+                        Wenn Sie Ihre Buchung stornieren möchten, müssen Sie dies innerhalb der angegebenen
+                        Stornierungsfrist
+                        tun, um eine unnötige Kreditkartenbelastung zu vermeiden.
+                        Wenn Sie vergessen, Ihre Reservierung innerhalb des geforderten Zeitraums zu stornieren, wird
+                        Ihnen
+                        das
+                        Hotel voraussichtlich eine Übernachtung in Rechnung stellen.
+
+                    </p>
+                </div>
+            </div>
+
+            <div class="accordion-item" id="question5">
+                <a class="accordion-link" href="#question5">
+                    Was ist zu tun, wenn ich keine Reservierungsbestätigung erhalte?
+                    <i class="icon ion-md-add"></i>
+                    <i class="icon ion-md-remove"></i>
+                </a>
+                <div class="answer">
+                    <p>
+                        Sollten Sie innerhalb von 48 Stunden keine Buchungsbestätigung erhalten, bitten wir Sie, sich
+                        direkt
+                        mit
+                        dem Hotel in Verbindung zu setzen.
+
+                    </p>
+                </div>
+
+
+</section>
