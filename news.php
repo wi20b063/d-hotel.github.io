dad73f03f6c6aa969390913e723d14f1ec512ce5
wi20b063 <84073745+wi20b063@users.noreply.github.com>
2022-11-18 16:54:35 +0100

    news page

--- news.php
+++ news.php
@@ -13,64 +13,110 @@
         <?php include "components/navbar.php";?>
     </nav>
 
-    <main>
+    <main>   
         <div class="content">
             <div class="container">
-                <div class="row">
-                    <div class="col-9">
+                <h1>News-Beiträge</h1>
+                <div class="row my-2 border border-1 rounded">
+                    <div>
+                        <p>Verfasser:in: Maria Mustermann | Datum: <?php echo date('d.m.Y')?> </p>
+                    </div>
+                    <div class="row">
+                        <div class=" text col-sm-8">                
+                            <h3>Lorem ipsum dolor sit amet</h3>
+                            <p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel
+                                illum
+                                dolore
+                                eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit
+                                praesent
+                                luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit
+                                amet,
+                                consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore
+                                magna
+                                aliquam
+                                erat volutpat.</p>
+                            <p>Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl
+                                ut
+                                aliquip ex
+                                ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse
+                                molestie
+                                consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto
+                                odio
+                                dignissim
+                                qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
+                        </div>
+                        <div class="image col-sm-3 d-flex align-items-sm-center">
+                            <img src="res\img\img news\placeholder image.png" alt="news1" class="img-fluid">
+                        </div>
+                    </div>
+                </div>
 
-                        <h3>Lorem ipsum dolor sit amet</h3>
-                        <p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel
-                            illum
-                            dolore
-                            eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit
-                            praesent
-                            luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit
-                            amet,
-                            consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore
-                            magna
-                            aliquam
-                            erat volutpat.</p>
-                        <p>Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl
-                            ut
-                            aliquip ex
-                            ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse
-                            molestie
-                            consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto
-                            odio
-                            dignissim
-                            qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
-                        <br>
+                <div class="row my-2 border border-1 rounded">
+                    <div>
+                        <p>Verfasser:in: Maria Mustermann | Datum: <?php echo date('d.m.Y')?> </p>
+                    </div>
+                    <div class="row">
+                        <div class=" text col-sm-8">                
+                            <h3>Lorem ipsum dolor sit amet</h3>
+                            <p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel
+                                illum
+                                dolore
+                                eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit
+                                praesent
+                                luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit
+                                amet,
+                                consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore
+                                magna
+                                aliquam
+                                erat volutpat.</p>
+                            <p>Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl
+                                ut
+                                aliquip ex
+                                ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse
+                                molestie
+                                consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto
+                                odio
+                                dignissim
+                                qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
+                        </div>
+                        <div class="image col-sm-3 d-flex align-items-sm-center">
+                            <img src="res\img\img news\placeholder image.png" alt="news1" class="img-fluid">
+                        </div>
+                    </div>
+                </div>    
 
-                        <h3>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam</h3>
-                        <p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel
-                            illum
-                            dolore
-                            eu feugiat nulla facilisis. At vero eos et accusam et justo duo dolores et ea rebum. Stet
-                            clita
-                            kasd
-                            gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit
-                            amet,
-                            consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore
-                            magna
-                            aliquyam
-                            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita
-                            kasd
-                            gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit
-                            amet,
-                            consetetur sadipscing elitr, At accusam aliquyam diam diam dolore dolores duo eirmod eos
-                            erat,
-                            et
-                            nonumy
-                            sed tempor et et invidunt justo labore Stet clita ea et gubergren, kasd magna no rebum.
-                            sanctus
-                            sea
-                            sed
-                            takimata ut vero voluptua. est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
-                            consetetur
-                            sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-                            erat.
-                        </p>
+
+                <div class="row my-2 border border-1 rounded">
+                    <div>
+                        <p>Verfasser:in: Maria Mustermann | Datum: <?php echo date('d.m.Y')?> </p>
+                    </div>
+                    <div class="row">
+                        <div class=" text col-sm-8">                
+                            <h3>Lorem ipsum dolor sit amet</h3>
+                            <p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel
+                                illum
+                                dolore
+                                eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit
+                                praesent
+                                luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit
+                                amet,
+                                consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore
+                                magna
+                                aliquam
+                                erat volutpat.</p>
+                            <p>Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl
+                                ut
+                                aliquip ex
+                                ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse
+                                molestie
+                                consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto
+                                odio
+                                dignissim
+                                qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
+                        </div>
+                        <div class="image col-sm-3 d-flex align-items-sm-center">
+                            <img src="res\img\img news\placeholder image.png" alt="news1" class="img-fluid">
+                        </div>
                     </div>
                 </div>
             </div>
