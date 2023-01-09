8a5f92989cc9cfb8b7b060911266ab4a5c381081
distant-hotel <91386847+distant-hotel@users.noreply.github.com>
2023-01-09 22:36:22 +0100

    Add files via upload

--- booking.php
+++ booking.php
@@ -1,32 +1,29 @@
-<?php include "components/session.php";?>
+<?php include "components/session.php"; ?>
 
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <?php include "components/head.php";?>
+    <?php include "components/head.php"; ?>
     <title>Distant Hotel | Über uns</title>
 </head>
 
 <body>
     <nav>
-        <?php include "components/navbar.php";?>
+        <?php include "components/navbar.php"; ?>
     </nav>
 
     <main>
         <div class="content">
-            <div class="container">
-                <h1 class="headline">Zimmer buchen</h1>
 
-
-            </div>
+            <?php include "components/bookingform.php"; ?>
         </div>
     </main>
 
     <footer>
-        <?php include "components/footer.php";?>
+        <?php include "components/footer.php"; ?>
     </footer>
 
 </body>
 
-</html> 
+</html>
